ab5c6d99e119e66239f33cd781311d087b08d8cb
Martin Moldsvor <martin@moldsvor.com>
2021-02-24 13:41:42 +0100

    ELSYS-44: Added threading to USB communication for running in background

--- timing/ble_usb_handler.py
+++ timing/ble_usb_handler.py
@@ -1,4 +1,7 @@
 from time import sleep
+
+from threading import Thread
+
 from serial import Serial
 from serial.tools.list_ports import comports
 from serial.tools.list_ports_common import ListPortInfo
@@ -7,38 +10,44 @@ from serial.serialutil import SerialException
 from typing import Optional
 
 
-def process_value(state: bytes):
-    if len(state):
-        value = int.from_bytes(state, byteorder='little')
+class CommunicationThread:
+    def __init__(self, ble_serial_number: Optional[str]  = '000000000000'):
+        self.ble_serial_number = ble_serial_number
 
-        touch = value & 1
-        motion = (value & 2) >> 1
-        identifier = (value & ~3) >> 2
+        thread = Thread(target=self.run, daemon=True)
+        thread.start()
 
-        print(f'0b{value:08b}')
-        print(f'Id: {identifier}, Touch: {touch}, Motion:{motion}')
+    def process_value(self, state: bytes):
+        if len(state):
+            value = int.from_bytes(state, byteorder='little')
 
+            touch = value & 1
+            motion = (value & 2) >> 1
+            identifier = (value & ~3) >> 2
 
-def connect_to_device(port: ListPortInfo):
-    with serial.Serial(port.device) as serial_device:
-        print(f'Connected to {serial_device.name}')
-        while True:        
-            process_value(serial_device.read())
+            print(f'0b{value:08b}')
+            print(f'Id: {identifier}, Touch: {touch}, Motion:{motion}')
 
+    def connect_to_device(self, port: ListPortInfo):
+        with Serial(port.device) as serial_device:
+            print(f'Connected to {serial_device.name}')
+            while True:        
+                self.process_value(serial_device.read())
 
-def main(ble_serial_number: Optional[str]  = '000000000000'):
-    while True:
-        try:
-            port, = (port for port in comports() if port.serial_number == ble_serial_number)
-            print(type(port))
-            connect_to_device(port)
-        except SerialException as e:
-            print(f'Lost connection to device {port.device}.')
-        except ValueError as e:
-            print(f'No device found with serial number {ble_serial_number}.')
-        print('Trying to connect...')
-        sleep(5)
+    def run(self):
+        while True:
+            try:
+                port, = (port for port in comports() if port.serial_number == self.ble_serial_number)
+                self.connect_to_device(port)
+            except SerialException as e:
+                print(f'Lost connection to device {port.device}.')
+            except ValueError as e:
+                print(f'No device found with serial number {self.ble_serial_number}.')
+            print('Trying to connect...')
+            sleep(5)
 
 
 if __name__ == '__main__':
-    main()
+    communication_thread = CommunicationThread()
+    while True:
+        sleep(1)
