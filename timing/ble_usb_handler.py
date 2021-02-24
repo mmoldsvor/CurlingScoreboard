02bdfd0d72e3896fbff1af3fd0d9dff01f94d8c1
Martin Moldsvor <martin@moldsvor.com>
2021-02-24 13:21:20 +0100

    ELSYS-44: Added connection and reconnection handling for USB communication

--- timing/ble_usb_handler.py
+++ timing/ble_usb_handler.py
@@ -1,22 +1,43 @@
+from time import sleep
 from serial import Serial
+from serial.tools.list_ports import comports
+from serial.tools.list_ports_common import ListPortInfo
+from serial.serialutil import SerialException
+
+from typing import Optional
 
 
-def process_value(bytes):
-    value = int.from_bytes(bytes, byteorder='little')
+def process_value(state: bytes):
+    if len(state):
+        value = int.from_bytes(state, byteorder='little')
 
-    touch = value & 1
-    motion = (value & 2) >> 1
-    identifier = (value & ~3) >> 2
+        touch = value & 1
+        motion = (value & 2) >> 1
+        identifier = (value & ~3) >> 2
 
-    print(f'0b{value:08b}')
-    print(f'Id: {identifier}, Touch: {touch}, Motion:{motion}')
+        print(f'0b{value:08b}')
+        print(f'Id: {identifier}, Touch: {touch}, Motion:{motion}')
 
 
-def main():
-    with Serial('COM11') as ser:
-        print(f'Connected to {ser.name}')
+def connect_to_device(port: ListPortInfo):
+    with serial.Serial(port.device) as serial_device:
+        print(f'Connected to {serial_device.name}')
         while True:        
-            process_value(ser.read())
+            process_value(serial_device.read())
+
+
+def main(ble_serial_number: Optional[str]  = '000000000000'):
+    while True:
+        try:
+            port, = (port for port in comports() if port.serial_number == ble_serial_number)
+            print(type(port))
+            connect_to_device(port)
+        except SerialException as e:
+            print(f'Lost connection to device {port.device}.')
+        except ValueError as e:
+            print(f'No device found with serial number {ble_serial_number}.')
+        print('Trying to connect...')
+        sleep(5)
 
 
 if __name__ == '__main__':
